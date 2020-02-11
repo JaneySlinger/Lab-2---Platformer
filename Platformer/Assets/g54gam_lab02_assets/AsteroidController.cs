df7d8136f78f1c832bde00a574ba493a556d988d
Unknown <janeyslinger@gmail.com>
2020-02-11 11:22:08 +0000

    Added moving asteroids, background, and win platform

--- Platformer/Assets/g54gam_lab02_assets/AsteroidController.cs
+++ Platformer/Assets/g54gam_lab02_assets/AsteroidController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class AsteroidController : MonoBehaviour
 {
+    public AnimationCurve curve;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(transform.position.x, curve.Evaluate((Time.time % curve.length)), transform.position.z);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        Application.LoadLevel(Application.loadedLevel);
     }
 }
